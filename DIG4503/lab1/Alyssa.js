60ce8c33bf1c8b0fbc3dea67a3fe0b0d8a16f38f
abarella951 <59658168+abarella951@users.noreply.github.com>
2021-01-21 13:56:40 -0500

    Add files via upload

---
+++ DIG4503/lab1/Alyssa.js
@@ -0,0 +1,9 @@
+const Person = require("./Person.js");
+
+class Rachel extends Person {
+    constructor(name, colors, bgColors) {
+        super(name, colors, bgColors);
+    }
+}
+
+module.exports = Alyssa;
---
+++ DIG4503/lab1/index.js
@@ -0,0 +1,13 @@
+const Alyssa = require("./Alyssa.js");
+
+let p = new Alyssa ("Alyssa", [
+    "#ffd9f6",
+    "#d9fff9",
+    "#ffffd9"
+], [
+    "#e3ffd9",
+    "#d9e9ff",
+    "#fff5d9"
+]);
+
+p.speak();
