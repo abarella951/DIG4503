067c62f3b2e293453432319faea17b70537073a2
abarella951 <abarella951@knights.ucf.edu>
2021-02-25 22:51:31 -0500

    main

--- DIG4503/lab1/Alyssa.js
+++
@@ -1,9 +0,0 @@
-const Person = require("./Person.js");
-
-class Rachel extends Person {
-    constructor(name, colors, bgColors) {
-        super(name, colors, bgColors);
-    }
-}
-
-module.exports = Alyssa;
